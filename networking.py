7b37e728f77b6ec966a47de9b62a53680ec0a8f1
kstonekuan <kingstonkuan@u.nus.edu>
2020-10-31 13:31:59 +0800

    abstract functions

---
+++ networking.py
@@ -0,0 +1,15 @@
+import socket
+import json
+
+# ~~~~~============== NETWORKING CODE ==============~~~~~
+def connect(exchange_hostname, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((exchange_hostname, port))
+    return s.makefile('rw', 1)
+
+def write_to_exchange(exchange, obj):
+    json.dump(obj, exchange)
+    exchange.write("\n")
+
+def read_from_exchange(exchange):
+    return json.loads(exchange.readline())
