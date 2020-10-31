faf80dd3de00c1affa2f8e2a72309e053bcda0db
Wira Azmoon <e0014172@u.nus.edu>
2020-10-31 13:14:48 +0800

    VALBZ

--- sample-bot.py
+++ sample-bot.py
@@ -17,7 +17,7 @@ from collections import deque
 team_name="NULLPOINTEREXCEPTION"
 # This variable dictates whether or not the bot is connecting to the prod
 # or test exchange. Be careful with this switch!
-test_mode = True
+test_mode = False
 
 # This setting changes which test exchange is connected to.
 # 0 is prod-like
@@ -29,12 +29,6 @@ prod_exchange_hostname="production"
 port=25000 + (test_exchange_index if test_mode else 0)
 exchange_hostname = "test-exch-" + team_name if test_mode else prod_exchange_hostname
 
-buy_orders = dict()
-sell_orders = dict()
-shares = dict()
-shares['BOND'] = 0
-counter = 0
-best_prices = dict()
 stockFairPrices = {"VALBZ" : 0, "GS": 0, "MS": 0, "WFC": 0}
 
 # ~~~~~============== NETWORKING CODE ==============~~~~~
@@ -81,7 +75,7 @@ def buy(buy_orders, counter, exchange, symbol, price, size):
         "size": size
         }
 
-    buy_orders.append(counter)
+    # buy_orders.append(counter)
     write_to_exchange(exchange, payload)
 
     return counter
@@ -98,7 +92,7 @@ def sell(sell_orders, counter, exchange, symbol, price, size):
         "size": size
         }
     
-    sell_orders.append(counter)
+    # sell_orders.append(counter)
     write_to_exchange(exchange, payload)
 
     return counter
@@ -141,46 +135,6 @@ def cancelPastOrders(sell_orders):
     if len(sell_orders) > 0: sell_orders.popleft()
 
 # ~~~~~============== MAIN LOOP ==============~~~~~
-def add_to_market(message):
-    symbol = message["symbol"]
-    if (len(message['buy']) > 0):
-        buy_price = message['buy'][0]
-    elif symbol in best_prices:
-        buy_price = best_prices[symbol][0]
-    else:
-        buy_price = (0,0)
-
-    if (len(message['buy']) > 0):
-        sell_price = message['buy'][0]
-    elif symbol in best_prices:
-        sell_price = best_prices[symbol][1]
-    else:
-        sell_price = (0,0)
-
-    best_prices[symbol] = (buy_price, sell_price)   
-    
-
-def check_etf(exchange, message):
-    symbol = message["symbol"]
-
-    if (symbol == "VALE" and "VALBZ" in best_prices) or (symbol == "VALBZ" and "VALE" in best_prices):
-        vale_buy_pricenum, vale_sell_pricenum = best_prices[symbol]
-        valbz_buy_pricenum, valbz_sell_pricenum = best_prices["VALBZ"]
-
-        vale_buy_price, vale_buy_num = vale_buy_pricenum
-        valbz_buy_price, valbz_buy_num = valbz_buy_pricenum
-
-        vale_sell_price, vale_sell_num = vale_sell_pricenum
-        valbz_sell_price, valbz_sell_num = valbz_sell_pricenum
-
-        vale_to_valbz_num = min(vale_sell_num, valbz_buy_num)
-        valbz_to_vale_num = min(valbz_sell_num, vale_buy_num)
-        if (valbz_to_vale_num * valbz_sell_price + 10 < valbz_to_vale_num * vale_buy_price):
-            convert_to(exchange, "VALE", vale_to_valbz_num)
-        elif vale_to_valbz_num * vale_sell_price + 10 < vale_to_valbz_num * valbz_buy_price:
-            convert_from(exchange, "VALE", vale_to_valbz_num)
-
-# ~~~~~============== MAIN LOOP ==============~~~~~
 
 def main():
     exchange = connect()
@@ -198,6 +152,10 @@ def main():
     sell_orders = deque
     while True:
         message = read_from_exchange(exchange)
+        if(message["type"] == "close"):
+            print("The round has ended")
+            break
+        
         if message['type'] == 'book':
             if message['symbol'] == 'BOND': print(message)
         elif message['type'] == 'trade': continue
@@ -214,12 +172,8 @@ def main():
                     counter = buy(buy_orders, counter, exchange, 'BOND', message['sell'][0][0], message['sell'][0][1])
                     shares['BOND'] += message['sell'][0][1]
                     print(shares)
-            if message['symbol'] == 'VALE' or message['symbol'] == 'VALBZ':
-                check_etf(exchange, message)
 
-        if(message["type"] == "close"):
-            print("The round has ended")
-            break
+        
 
 if __name__ == "__main__":
     main()
