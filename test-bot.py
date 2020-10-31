9abce7914318a7e9ff2ace338a226ebe2c0ae883
daniellimzj <daniellimzijie@gmail.com>
2020-10-31 13:37:38 +0800

    change test mode

--- test-bot.py
+++ test-bot.py
@@ -17,7 +17,7 @@ from collections import deque
 team_name="NULLPOINTEREXCEPTION"
 # This variable dictates whether or not the bot is connecting to the prod
 # or test exchange. Be careful with this switch!
-test_mode = False
+test_mode = True
 
 # This setting changes which test exchange is connected to.
 # 0 is prod-like
