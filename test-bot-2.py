6ed7bd7a69b2b9b7ac2b9919923d87f3c0689f15
Wira Azmoon <e0014172@u.nus.edu>
2020-10-31 13:43:56 +0800

    ETF valb

--- test-bot-2.py
+++ test-bot-2.py
@@ -17,7 +17,7 @@ from collections import deque
 team_name="NULLPOINTEREXCEPTION"
 # This variable dictates whether or not the bot is connecting to the prod
 # or test exchange. Be careful with this switch!
-test_mode = False
+test_mode = True
 
 # This setting changes which test exchange is connected to.
 # 0 is prod-like
@@ -33,6 +33,8 @@ buy_orders = dict()
 sell_orders = dict()
 shares = dict()
 shares['BOND'] = 0
+shares['VALE'] = 0
+shares['VALBZ'] = 0
 counter = 0
 best_prices = dict()
 stockFairPrices = {"VALBZ" : 0, "GS": 0, "MS": 0, "WFC": 0}
@@ -65,9 +67,13 @@ def convert(counter, exchange, symbol, size, dir):
     return counter
 
 def convert_to(counter, exchange, symbol, size):
+    shares[symbol] += size
+    shares['VALBZ'] -= size
     return convert(counter, exchange, symbol, size, "BUY")
 
 def convert_from(counter, exchange, symbol, size):
+    shares[symbol] -= size
+    shares['VALBZ'] += size
     return convert(counter, exchange, symbol, size, "SELL")
     
 def buy(buy_orders, counter, exchange, symbol, price, size):
@@ -165,7 +171,7 @@ def check_etf(counter, exchange, message):
     symbol = message["symbol"]
 
     if (symbol == "VALE" and "VALBZ" in best_prices) or (symbol == "VALBZ" and "VALE" in best_prices):
-        vale_buy_pricenum, vale_sell_pricenum = best_prices[symbol]
+        vale_buy_pricenum, vale_sell_pricenum = best_prices["VALE"]
         valbz_buy_pricenum, valbz_sell_pricenum = best_prices["VALBZ"]
 
         vale_buy_price, vale_buy_num = vale_buy_pricenum
@@ -174,11 +180,13 @@ def check_etf(counter, exchange, message):
         vale_sell_price, vale_sell_num = vale_sell_pricenum
         valbz_sell_price, valbz_sell_num = valbz_sell_pricenum
 
-        vale_to_valbz_num = min(vale_sell_num, valbz_buy_num)
-        valbz_to_vale_num = min(valbz_sell_num, vale_buy_num)
-        if (valbz_to_vale_num * valbz_sell_price + 10 < valbz_to_vale_num * vale_buy_price):
+        vale_to_valbz_num = min(vale_sell_num, valbz_buy_num, shares['VALE'])
+        valbz_to_vale_num = min(valbz_sell_num, vale_buy_num, shares['VALBZ'])
+        if valbz_to_vale_num * valbz_sell_price + 10 < valbz_to_vale_num * vale_buy_price \
+            and valbz_to_vale_num > 0:
             counter = convert_to(counter, exchange, "VALE", vale_to_valbz_num)
-        elif vale_to_valbz_num * vale_sell_price + 10 < vale_to_valbz_num * valbz_buy_price:
+        elif vale_to_valbz_num * vale_sell_price + 10 < vale_to_valbz_num * valbz_buy_price \
+            and vale_to_valbz_num > 0:
             counter = convert_from(counter, exchange, "VALE", vale_to_valbz_num)
     return counter
 
@@ -196,12 +204,15 @@ def main():
     shares = dict()
     shares['BOND'] = 0
     counter = 0
-    buy_orders = deque
-    sell_orders = deque
+    buy_orders = deque()
+    sell_orders = deque()
     while True:
         message = read_from_exchange(exchange)
         if message['type'] == 'book':
+            add_to_market(message)
+            print(best_prices)
             if message['symbol'] == 'BOND': print(message)
+
         elif message['type'] == 'trade': continue
         else:
             print(message)
