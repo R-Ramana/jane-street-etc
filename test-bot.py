0dbc7aef589894630c0ab0ce1638c6fc461ba5e8
daniellimzj <daniellimzijie@gmail.com>
2020-10-31 14:35:24 +0800

    Remove valbz from stocks

--- test-bot.py
+++ test-bot.py
@@ -35,9 +35,9 @@ shares = dict()
 shares['BOND'] = 0
 counter = 0
 best_prices = dict()
-stockFairPrices = {"VALBZ" : 0, "GS": 0, "MS": 0, "WFC": 0}
+stockFairPrices = {"GS": 0, "MS": 0, "WFC": 0}
 
-stocks = ["VALBZ", "GS", "MS", "WFC"]
+stocks = ["GS", "MS", "WFC"]
 
 # ~~~~~============== NETWORKING CODE ==============~~~~~
 def connect():
@@ -211,7 +211,6 @@ def main():
     print("The exchange replied:", hello_from_exchange, file=sys.stderr)
     shares = dict()
     shares['BOND'] = 0
-    shares['VALBZ'] = 0
     shares['GS'] = 0
     shares['MS'] = 0
     shares['WFC'] = 0
