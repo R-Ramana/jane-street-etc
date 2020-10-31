4583715abb753ea85df275040cb2f89e3182a751
kstonekuan <kingstonkuan@u.nus.edu>
2020-10-31 15:12:01 +0800

    e

--- bond.py
+++ bond.py
@@ -5,9 +5,11 @@ def sellBondHigherThanFairPrice(sell_orders, counter, exchange, message, shares)
         counter = sell(sell_orders, counter, exchange, 'BOND', message['buy'][0][0], message['buy'][0][1])
         shares['BOND'] -= message['buy'][0][1]
         print(shares)
+    return counter
 
 def buyBondLowerThanFairPrice(buy_orders, counter, exchange, message, shares):
     if len(message['sell']) > 0 and message['sell'][0][0] < 1000 and shares['BOND'] < 100:
         counter = buy(buy_orders, counter, exchange, 'BOND', message['sell'][0][0], message['sell'][0][1])
         shares['BOND'] += message['sell'][0][1]
         print(shares)
+    return counter
